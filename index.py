5565b1fc1d607d24308f8eca2bd49b7c9a5497fc
Robert Scruggs <scruggstyrese@gmail.com>
2023-01-04 12:32:06 -0500

    practice with buttons, need more practice

--- index.py
+++ index.py
@@ -1,14 +1,31 @@
-import random
+import tkinter as tk
 
-randomNumber = random.randint(2,5)
-player = int(input("Whats the number? "))
+# window
+window = tk.Tk()
+window.title("Practice With Tkinter")
+window.geometry("500x500")
+window.configure(bg='lightgray')
+
+#frame
+main_frame = tk.Frame(window, width=200, height=400)
+main_frame.grid(row=0, column=0, padx=10, pady=5)
+
+second_frame = tk.Frame(window, width=300, height=300, bg="purple")
+second_frame.grid(row=0, column=1, padx=0, pady=0)
+second_frame.pack_propagate(False)
+
+#label
+tk.Label(main_frame, text="Fat Bitches").grid(row=0,column=0)
 
 
-while player != randomNumber:
-    print("Wrong!")
-    player = int(input("Whats the number? "))
+
+button = tk.Button(second_frame, text="Click me!", width=20)
+button.grid(row=1, column=0, padx=10, pady=5)
+
+button2 = tk.Button(second_frame, text="Click me 2!", width=20)
+button2.grid(row=2, column=0, padx=10, pady=5)
 
 
 
 
-print("All finished thanks for playing!")
+window.mainloop()
