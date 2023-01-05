74dd2c21a011b381ed1e68d9506657dd5c41ee3c
Robert Scruggs <scruggstyrese@gmail.com>
2023-01-05 11:24:37 -0500

    NA

--- index.py
+++ index.py
@@ -1,29 +1,55 @@
+from cgitb import text
 import tkinter as tk
+
+def showMessage():
+    print(textbox.get("1.0", tk.END))
+    textbox.delete("1.0",tk.END)
+
 
 # window
 window = tk.Tk()
 window.title("Practice With Tkinter")
 window.geometry("500x500")
-window.configure(bg='lightgray')
+window.configure(bg='darkgray')
 
 #frame
-main_frame = tk.Frame(window, width=200, height=400)
-main_frame.grid(row=0, column=0, padx=10, pady=5)
 
-second_frame = tk.Frame(window, width=300, height=300, bg="purple")
-second_frame.grid(row=0, column=1, padx=0, pady=0)
-second_frame.pack_propagate(False)
+label = tk.Label(window, text="Good Morning", font=('Arial', 18))
+label.pack(padx=20,pady=20)
 
-#label
-tk.Label(main_frame, text="Fat Bitches").grid(row=0,column=0)
+textbox = tk.Text(window, height=3, font=('Arial', 16))
+textbox.pack(padx = 10, pady=10)
+
+button = tk.Button(window, text="Click on me!", font=('Arial', 18))
+button.pack(padx=10, pady=10)
+
+mainFrame = tk.Frame(window, width=300,height=300, bg="Black")
+mainFrame.pack(padx=10, pady=10)
+
+button1 = tk.Button(mainFrame, text="Button1", command=showMessage)
+button1.grid(row=0, column=0, padx=5, pady=5)
+
+button2 = tk.Button(mainFrame, text="Button2")
+button2.grid(row=0, column=1, padx=5, pady=5)
+
+button3 = tk.Button(mainFrame, text="Button3")
+button3.grid(row=0, column=2, padx=5, pady=5)
+
+button4 = tk.Button(mainFrame, text="Button4")
+button4.grid(row=1, column=0, padx=5, pady=5)
+
+button5 = tk.Button(mainFrame, text="Button5")
+button5.grid(row=1, column=1, padx=5, pady=5)
+
+button6 = tk.Button(mainFrame, text="Button6")
+button6.grid(row=1, column=2, padx=5, pady=5)
 
 
 
-button = tk.Button(second_frame, text="Click me!", width=20)
-button.grid(row=1, column=0, padx=10, pady=5)
 
-button2 = tk.Button(second_frame, text="Click me 2!", width=20)
-button2.grid(row=2, column=0, padx=10, pady=5)
+
+
+
 
 
 
