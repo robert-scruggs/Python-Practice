96ce5ef40bb58f61fa320d75041587e1dfd1a377
Robert Scruggs <scruggstyrese@gmail.com>
2023-01-07 16:47:00 -0500

    NA

--- index.py
+++ index.py
@@ -1,37 +1,36 @@
-from cgitb import text
-import tkinter as tk
-import requests as r
-from bs4 import BeautifulSoup
 
+from tkinter import *
+from turtle import bgcolor
 
-url = "https://www.Wizard101.com"
-result = r.get(url)
-trans = BeautifulSoup(result.text, "html.parser")
-pretty = trans.prettify()
-# window
-window = tk.Tk()
-window.title("Practice With Tkinter")
-window.geometry("750x750")
-window.configure(bg='#66545e')
-#frame
+def submit():
+    username = entry.get()
+    print("Hello " + username)
 
-frame1 = tk.Frame(window, width=100, height=100, bg='#a39193').place(relx=.5, rely=.5,anchor=tk.CENTER)
-messageLabel = tk.Label(window, text=pretty)
-messageLabel.pack(padx=30,pady=30);
+def delete():
+    entry.delete(0,END)
 
+def backspace():
+    entry.delete(len(entry.get()) - 1, END)
 
+window = Tk()
+window.geometry("300x250")
+window.title("Python Type Beat")
+window.config(bg="black")
 
+entry = Entry(window,
+            font=("Arial", 50),
+            fg="orange",
+            bg="black")
+entry.insert(0, "SpongeBob")
+entry.pack(side=LEFT)
 
+submit_Button = Button(window, text="submit", command=submit)
+submit_Button.pack(side=RIGHT)
 
+delete_Button = Button(window, text="Delete", command=delete)
+delete_Button.pack(side=RIGHT)
 
-
-
-
-
-
-
-
-
-
+backspace_Button = Button(window, text="Backspace", command=backspace)
+backspace_Button.pack(side=RIGHT)
 
 window.mainloop()
