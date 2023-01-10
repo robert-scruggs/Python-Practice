bdaa6ffe586958280850e29e04d63b2e58310980
Robert Scruggs <scruggstyrese@gmail.com>
2023-01-10 13:13:42 -0500

    more pracitce

--- custom.py
+++
@@ -1,8 +0,0 @@
-import customtkinter 
-
-customtkinter.set_appearance_mode("Dark")
-
-customtkinter.set_default_color_theme("dark-blue")
-
-
-customtkinter.mainloop()
--- url.py
+++ url.py
@@ -1,4 +1,4 @@
-import tkinter
+
 import mysql.connector
 from tkinter import *
 
